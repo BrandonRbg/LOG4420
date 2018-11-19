c006025dfe10f6c0748dd93f77b557f84c4f25c6
JulienDuf <dufresne_julien@hotmail.ca>
2018-11-19 15:48:28 -0500

    Added index

--- LOG4420_TP4/routes/index.js
+++ LOG4420_TP4/routes/index.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
 });
 
+router.get("/accueil", (req, res) => {
+  res.render("index", { title: "Accueil" });
+});
+
 module.exports = router;
