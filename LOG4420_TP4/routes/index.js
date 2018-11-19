61ab2718311f3632530ce048821df148c19f1868
Brandon Roberge <brandonroberge@hotmail.com>
2018-11-19 16:24:13 -0500

    Stuff

--- LOG4420_TP4/routes/index.js
+++ LOG4420_TP4/routes/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+  res.render("index", {title: "Accueil", message: "Ça semble fonctionner!"});
 });
 
 router.get("/accueil", (req, res) => {
-  res.render("index", { title: "Accueil" });
+  res.render("index", {title: "Accueil"});
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
